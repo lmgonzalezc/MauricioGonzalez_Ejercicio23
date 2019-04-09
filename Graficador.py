e7cf18ebb5ef59f6c37262cbdf6822f43e1a801c
Mauricio González <46940813+lmgonzalezc@users.noreply.github.com>
2019-04-09 14:22:32 -0500

    Ult version

--- Graficador.py
+++ Graficador.py
@@ -3,9 +3,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-os.system("g++ Montecarlo.cpp -o Montecarlo.x")
-os.system("./Montecarlo.x > datos.dat")
+os.system("g++ Suma.cpp -o Suma.x")
+os.system("./Suma.x > datos.dat")
 
 data = np.loadtxt("datos.dat")
 
-print(data[:,0], data[:,1])
+print(data)
